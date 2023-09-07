bb2d8f5315a000a4e80975b140823458dea76f75
Lyle Xu <chanxl009@gmail.com>
2023-09-07 14:22:43 +0800

    init

---
+++ WebApplication1/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.Label1.Text = Request.QueryString["TextBox1"];
+        }
+
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Default.aspx?TextBox1=" + TextBox1.Text);
+        }
+    }
+}
