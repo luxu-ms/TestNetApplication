21a5438043ea3da356cc0064eff03c3bed825acc
Lyle Xu <chanxl009@gmail.com>
2023-09-07 14:52:21 +0800

    add sqlcommand

---
+++ WebApplication1/TestDBHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class TestDBHelper
+    {
+        void testSqlCommand()
+        {
+            SqlCommand cmd = new SqlCommand();
+        }
+    }
+}
